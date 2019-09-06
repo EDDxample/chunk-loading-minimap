4c0e57c9092210907857fc40b1e68683c2c61b4e
EDDxample <EDDxample@gmail.com>
2019-09-06 15:36:46 +0200

    fix Main.java

---
+++ src/main/java/eddxample/chunkminimap/Main.java
@@ -0,0 +1,11 @@
+package eddxample.chunkminimap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Hello Fabric world!");
+	}
+}
